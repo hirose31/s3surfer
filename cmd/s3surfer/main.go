3bf65beb2ec7807d97b67b1bd328ecc0891ed053
HIROSE Masaaki <hirose31@gmail.com>
2021-10-25 18:23:32 +0900

    Resolve path of os.Args[0] before calling syscall.Exec

--- cmd/s3surfer/main.go
+++ cmd/s3surfer/main.go
@@ -7,6 +7,7 @@ import (
 	"syscall"
 
 	"github.com/alecthomas/kong"
+	"github.com/cli/safeexec"
 
 	"github.com/hirose31/s3surfer/pkg/c"
 )
@@ -41,7 +42,12 @@ func init() {
 	if os.Getenv("LC_CTYPE") != "en_US.UTF-8" {
 		os.Setenv("LC_CTYPE", "en_US.UTF-8")
 		env := os.Environ()
-		if err := syscall.Exec(os.Args[0], os.Args, env); err != nil {
+		argv0, err := safeexec.LookPath(os.Args[0])
+		if err != nil {
+			panic(err)
+		}
+		os.Args[0] = argv0
+		if err := syscall.Exec(argv0, os.Args, env); err != nil {
 			panic(err)
 		}
 	}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.5.0 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.3.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.11.1 // indirect
+	github.com/cli/safeexec v1.0.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.4.0 // indirect
 	github.com/rivo/tview v0.0.0-20210624165335-29d673af0ce2 // indirect
