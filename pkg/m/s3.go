f2f4222d22ce78a71a4e61bd755b760ea78d4ad0
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-12-17 19:27:05 +0900

    ditto

--- pkg/m/s3.go
+++ pkg/m/s3.go
@@ -25,6 +25,7 @@ type optsFunc = func(*config.LoadOptions) error
 // S3Model ...
 type S3Model struct {
 	bucket           string
+	pathStyle        bool
 	availableBuckets []S3Bucket
 	prefix           string
 	client           *s3.Client
@@ -117,6 +118,8 @@ func NewS3Model(endpointURL string, region string, pathStyle bool) *S3Model {
 	// cache
 	s3m.cache = map[string]*ObjectCache{}
 
+	s3m.pathStyle = pathStyle
+
 	return &s3m
 }
 
@@ -145,8 +148,9 @@ func (s3m *S3Model) SetBucket(bucket string) error {
 		if s3m.endpointURL != "" {
 			endpoint := aws.EndpointResolverFunc(func(service, r string) (aws.Endpoint, error) {
 				return aws.Endpoint{
-					URL:           s3m.endpointURL,
-					SigningRegion: r,
+					URL:               s3m.endpointURL,
+					SigningRegion:     r,
+					HostnameImmutable: s3m.pathStyle,
 				}, nil
 			})
 			opts = append(opts, config.WithEndpointResolver(endpoint))
